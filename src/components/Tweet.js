64f4f8b4f41845b88452194f1b87fca0aa6d851f
Leonardo Nunes <itsmeleo_n@outlook.com>
2022-08-30 10:09:23 -0300

    lab 02

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,37 @@
-function Tweet() {
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+import Actions from "./Actions";
+
+function Tweet({tweet}) {
+
+  console.log(tweet)
+
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+     <ProfileImage image={tweet.user.image}/>
+     
 
       <div className="body">
         <div className="top">
           <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
+           <User user={tweet.user.name}/>
+           <User handle={tweet.user.handle}/>
           </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+         <Timestamp timestamp={tweet.timestamp}/>
+          
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+       <Message message={tweet.message}/>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Actions/>
       </div>
 
-      <i class="fas fa-ellipsis-h"></i>
+      <i className="fas fa-ellipsis-h"></i>
     </div>
   );
-}
+};
 
 export default Tweet;
